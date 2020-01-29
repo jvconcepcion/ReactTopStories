5f93d1ec36dd5d509b5af3dbc5c1a08e63607012
Jonathan Concepcion <jonathan.concepcion@rappler.com>
2020-01-29 19:15:19 +0800

    fix embed page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import { CarouselR5Style } from '../components/Carousel';
+// import { CarouselR5Style } from '../components/Carousel';
 import { Wrapper } from '../components/Wrapper';
 
 const Index = () => {
 	return (
 		<Wrapper embedBar='true'>
-			<CarouselR5Style api='https://svc.rappler.com/p/topstories' />
+			test
+			{/* <CarouselR5Style api='https://svc.rappler.com/p/topstories' /> */}
 		</Wrapper>
 	);
 };
